16a972f06a09d666405c94d8f605899e40e027ae
Hussein, Youssef <youssefmohamed552@gmail.com>
2021-02-11 22:08:45 -0500

    creating kb infobot class

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import sys
-from data_map import DataMap
-from featured_question import encode_statement
-from belief_tracker import BeliefTracker
+from kb_infobot import KB_infoBot
 
 def main():
-  input_file = 'glove.6B.100d.txt'
-  data_map = DataMap(input_file)
-  word_map = data_map.get_map()
-  print('word embeddings parsed in the system')
+  ib = KB_infoBot()
   question = 'where are the pyramids'
-  encoded_question = encode_statement(question, word_map)
-  belief_tracker = BeliefTracker(100, 256)
-  belief = belief_tracker.forward(encoded_question)
-  print(belief)
+  ib.eval(question)
 
 
   
